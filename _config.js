d3fec047543e547da05e47eb111542a567272771
Igor Anić <igor.anic@gmail.com>
2023-02-06 18:32:52 +0100

    move stanovi hbs to njk

--- _config.js
+++ _config.js
@@ -7,8 +7,34 @@ function currency(num, digits) {
   return num.toLocaleString('hr', { minimumFractionDigits: digits, maximumFractionDigits: digits });
 }
 
+function kat(num) {
+  return num ? num : "PR"
+}
+
+function toID(code) {
+  return code.replace(" ", "-").toLowerCase()
+}
+
+export function numeric(a, digits) {
+  if (!a) {
+    return "-";
+  }
+  if (!Array.isArray(a)) {
+    return currency(a, digits);
+  }
+  if (a.length == 1) {
+    return currency(a[0], digits);
+  }
+  return a.map(e => currency(e, digits)).join(" | ");
+}
+
+
 const site = lume();
+site.copy("assets", ".");
 site.filter("currency", currency);
+site.filter("numeric", numeric);
+site.filter("kat", kat);
+site.filter("toID", toID);
 
 site.use(minifyHTML());
 export default site;
